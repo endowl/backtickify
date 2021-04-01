2dac294c4dae9a47ff308277fe5cdba2259d2255
bgok <ken@citizenhodler.com>
2021-04-01 12:50:50 -0700

    Trying stuff

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import dataStore from "./src/dataStore.mjs"
-import helpers from "./src/helpers.mjs"
+import * as dataStore from "./src/dataStore.mjs"
+import * as helpers from "./src/helpers.mjs"
 import T from "./src/Templatize.mjs"
 
 export {T, helpers, dataStore}
