848366d0f72debc4e4b1958189f308cc38e899e1
bgok <ken@citizenhodler.com>
2021-04-23 15:14:07 -0700

    Clean up props on the data object bump version

--- src/dataStore.mjs
+++ src/dataStore.mjs
@@ -1,12 +1,38 @@
 import _get from "lodash/get.js";
+import isPlainObject from "lodash/isPlainObject";
+import isArray from "lodash/isArray";
 
 // Singleton data provider
 
 let data
 
-export const set = r => data = r
+function removeEmptyProps(s) {
+    if (isPlainObject(s)) {
+        return Object.keys(s).reduce((acc, k) => {
+            if (isPlainObject(s[k])) {
+                const t = removeEmptyProps(s[k])
+                if (Object.keys(t).length) {
+                    acc[k] = t
+                }
+            } else if (isArray(s[k])) {
+                acc[k] = s[k].map(removeEmptyProps)
+            } else if (s[k]) {
+                acc[k] = s[k]
+            }
+            return acc
+        }, {})
+    }
+    return s
+}
+
+export const set = r => data = removeEmptyProps(r)
 export const get = p => _get(data, p)
-export const getById = (p, k) => get(p)?.find(e => e.id === k)
+export const getById = (p, k) => {
+    const result =  get(p)?.find(e => {
+        return e.id === k
+    })
+    return result
+}
 export const rootIsSet = () => !!data
 
 export default {set, get, getById, rootIsSet}
